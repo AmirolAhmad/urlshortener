bbf64d835750d7772e85b8f656147039c8c91c0d
AmirolAhmad <9w2pdf@gmail.com>
2015-03-16 00:40:05 +0800

    refactor assets path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Enable the asset pipeline
+Rails.application.config.assets.enabled = true
+
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "swfs")
+Rails.application.config.assets.initialize_on_precompile = false
